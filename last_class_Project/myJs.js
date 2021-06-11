35340cdfebf34069aa8e8d474e8ed34d046da972
Oro1st <79486412+Oro1st@users.noreply.github.com>
2021-06-11 09:36:22 -0500

    Display and getting user function

--- last_class_Project/myJs.js
+++ last_class_Project/myJs.js
@@ -1,11 +1,28 @@
-window.onload = function () {
-    document.getElementById("search-bu").onclick = fetchUser
-    async function fetchUser() {
-        let userId = Number(document.getElementById("input-text").value)
-        let result = await fetch("https://jsonplaceholder.typicode.com/users");
-        let userFetch = await result.json();
-        let filterUser = userFetch.filter(u => u.id === userId)
-        console.log(filterUser)
 
-    }
-}
+
+window.onload = function pageOnload() {
+    const search = document.getElementById("search-user");
+    const { from } = rxjs;
+    const { filter } = rxjs.operators;
+    search.onclick = printUser;
+
+    // print user function
+    function printUser() {
+        const userId = Number(document.getElementById("user-id").value);
+        let userInfo = document.getElementById("user-list");
+        fetchUser();
+
+        // getting user information
+
+        async function fetchUser() {
+            userInfo.innerHTML = "";
+            let result = await fetch("https://jsonplaceholder.typicode.com/users");
+            let userFetch = await result.json();
+            const user = from(userFetch);
+            user
+                .pipe(filter((element) => element.id === userId))
+                .subscribe((data) => {
+                    printUserData(data);
+                });
+        }
+
